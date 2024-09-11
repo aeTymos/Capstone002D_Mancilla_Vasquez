d252ecd34f2734cc7fd9b0e44abf2473fb3b92dd
aeTymos <l.vasquez2@duocuc.cl>
2024-09-11 13:26:49 -0300

    Check for existing databases

---
+++ FASE 2/Evidencias Proyecto/proyecto/capstone_site/db_check.py
@@ -0,0 +1,34 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+from django.core.exceptions import ImproperlyConfigured
+
+load_dotenv()
+
+DB_CONFIGS = [
+        {
+            'dbname': os.environ.get('DB_NAME'),
+            'user': os.environ.get('DB_USER'),
+            'password': os.environ.get('DB_PASSWORD'),
+            'host': os.environ.get('DB_HOST'),
+            'port': os.environ.get('DB_PORT'),
+            
+        },
+        {
+            'name': os.environ.get('DB_NAME_BACKUP'),
+            'user': os.environ.get('DB_USER_BACKUP'),
+            'password': os.environ.get('DB_PASSWORD_BACKUP'),
+            'host': os.environ.get('DB_HOST_BACKUP'),
+            'port': os.environ.get('DB_PORT_BACKUP'),
+        }
+    ]
+
+def check_connection(db_configs):
+
+    try:
+        conn = psycopg2.connect(**db_configs)
+        conn.close()
+        return True
+    except Exception as e:
+        print(f"Fallo en la conexión a la base de datos. Error: {e}")
+        return False
